ff2e9b2e224476f04123dd1b259287890d05c23d
CyradisNyx <sarahbeavan@gmail.com>
2021-04-03 17:17:50 +1300

    Basic terrible movement
    Not turn-based, but grid-locked (ish)

---
+++ Assets/Scripts/GridController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridController : MonoBehaviour
+{
+    public PlayerController player;
+    public GameObject highlightPrefab;
+
+    private Grid grid;
+    private List<GameObject> highlightedTiles;
+
+    private void Start()
+    {
+        grid = gameObject.GetComponent<Grid>();
+        highlightedTiles = new List<GameObject>();
+    }
+
+    private void Update()
+    {
+        ClearHighlighted();
+        HighlightTile(GetPlayerPosition());
+    }
+
+    void ClearHighlighted()
+    {
+        foreach (var highlightedTile in highlightedTiles)
+        {
+            Destroy(highlightedTile);
+        }
+        highlightedTiles.Clear();
+    }
+
+    void HighlightTile(Vector3Int cell)
+    {
+        Vector3 worldPos = grid.CellToWorld(cell);
+        GameObject obj = Instantiate(highlightPrefab, worldPos, Quaternion.identity);
+        highlightedTiles.Add(obj);
+    }
+
+    Vector3Int GetPlayerPosition()
+    {
+        return grid.WorldToCell(player.transform.position);
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public bool selected = true;
+    public float moveSpeed = 5f;
+    public Transform movePoint;
+
+    private void Start()
+    {
+        movePoint.parent = null;
+    }
+
+    private void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, movePoint.position, moveSpeed * Time.deltaTime);
+
+        if (Vector3.Distance(transform.position, movePoint.position) <= .05f)
+        {
+            if (Mathf.Abs(Input.GetAxisRaw("Horizontal")) == 1f)
+            {
+                movePoint.position += new Vector3(Input.GetAxisRaw("Horizontal"), 0, 0);
+            }
+            if (Mathf.Abs(Input.GetAxisRaw("Vertical")) == 1f)
+            {
+                movePoint.position += new Vector3(0, Input.GetAxisRaw("Vertical"), 0);
+            }
+        }
+    }
+}
