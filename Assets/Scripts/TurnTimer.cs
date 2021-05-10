98b1ab1c066282abf3dda9e68957351bff58b9eb
CyradisNyx <sarahbeavan@gmail.com>
2021-05-11 05:07:36 +1200

    TurnTimer implemented

---
+++ Assets/Scripts/TurnTimer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TurnTimer : MonoBehaviour
+{
+    public Text turnText;
+    public int currentTurn = 0;
+    public int maxTurn = 5;
+
+    public void Start()
+    {
+        if (turnText == null)
+        {
+            turnText = transform.GetComponentInChildren<Text>();
+        }
+        turnText.text = $"Turn {currentTurn}/{maxTurn}";
+
+        EventMaster.Instance.ONEndTurn += OnEndTurn;
+    }
+
+    public void OnEndTurn()
+    {
+        currentTurn++;
+        turnText.text = $"Turn {currentTurn}/{maxTurn}";
+        if (currentTurn == maxTurn)
+        {
+            EventMaster.Instance.FailLevel();
+        }
+    }
+}
